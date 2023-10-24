84e91e1e56f8ffc152b3f085d23bd596022ce83e
LonelyNight11 <90082964+LonelyNight11@users.noreply.github.com>
2023-10-24 23:18:20 +0900

    [Bronze V] Title: 등록, Time: 108 ms, Memory: 13900 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println(62);
+		System.out.println("assameasyou11");
+	}
+}

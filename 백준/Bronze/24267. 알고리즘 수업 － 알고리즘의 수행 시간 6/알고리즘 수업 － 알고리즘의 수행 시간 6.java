b9ed868a31ebe4b4251da03694aae3b4eee584a6
점점 마음이 이끌려 <90082964+LonelyNight11@users.noreply.github.com>
2023-10-28 17:38:04 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -0,0 +1,19 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        long N = Integer.parseInt(bf.readLine());
+
+        long result = N * (N-1) * (N-2) / 6 ;
+        System.out.println(result);
+        System.out.println(3);
+    }
+}

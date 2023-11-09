3420063ba20bb6b7a1d4d0f6dd31d614d9f0aae2
점점 마음이 이끌려 <90082964+LonelyNight11@users.noreply.github.com>
2023-11-09 20:46:32 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 120 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,25 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
+
+
 public class Main {
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr = new int[9];
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] array = new int[9];
         int sum = 0;
         for (int i = 0; i < 9; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-            sum += arr[i];
+            array[i] = Integer.parseInt(bf.readLine());
+            sum += array[i];
         }
         for (int i = 0; i < 8; i++) {
-            for (int j = i+1; j < 9; j++) {
-                if (sum - arr[i] - arr[j] == 100) {
-                    arr[i] = 0;
-                    arr[j] = 0;
-                    Arrays.sort(arr);
+            for (int j = i + 1; j < 9; j++) {
+                if (100 == sum - array[i] - array[j]) {
+                    array[j] = 0;
+                    array[i] = 0;
+
+                    Arrays.sort(array);
                     for (int k = 2; k < 9; k++) {
-                        System.out.println(arr[k]);
+                        System.out.println(array[k]);
                     }
                     return;
                 }
+
             }
         }
     }

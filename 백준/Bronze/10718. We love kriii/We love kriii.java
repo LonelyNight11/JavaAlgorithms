9ef5347c5ddffab7cbaab4bcd88ef9a1fa6fd329
점점 마음이 이끌려 <90082964+LonelyNight11@users.noreply.github.com>
2023-11-20 22:45:05 +0900

    [Bronze V] Title: We love kriii, Time: 116 ms, Memory: 13836 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+
+
+}
